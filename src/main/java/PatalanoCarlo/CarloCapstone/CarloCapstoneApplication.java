dcf3b231264da17ee8a84e7b25a35faa0377ac96
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-07-22 14:26:51 +0200

    Initial commit

---
+++ src/main/java/PatalanoCarlo/CarloCapstone/CarloCapstoneApplication.java
@@ -0,0 +1,13 @@
+package PatalanoCarlo.CarloCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarloCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarloCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PatalanoCarlo/CarloCapstone/CarloCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package PatalanoCarlo.CarloCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarloCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
